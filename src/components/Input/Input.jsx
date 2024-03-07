dd47e1b6fe3eb5f949324f16dcecaac746a11531
yusufciftci <yusufciftci@outlook.com.tr>
2024-03-07 14:30:45 +0300

    add input styles

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,7 +3,7 @@ const Input = ({ type, placeholder, value, setLang, disabled }) => {
   return (
     <textarea
       rows="6"
-      cols="50"
+      cols="40"
       type={type}
       placeholder={placeholder}
       value={value}
